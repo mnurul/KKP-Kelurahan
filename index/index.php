d951196f2b6d8ad8798f2262bbc3dfe13ca3dd42
mnurul <mnurulislam05@gmail.com>
2019-03-17 14:32:36 +0700

    Index

---
+++ index/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shourcut icon" href="../image/logo pemkot bekasi-3.png">
+    <title>Kelurahan Harapan Baru</title>
+    <link rel="stylesheet" href="../bs/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="bg">
+        <img src="../image/logo pemkot bekasi-3.png" class="logo" alt="">
+        <img src="../image/lineup-3.png" class="lineup" alt="">         
+        <h5 class="title" >KELURAHAN HARAPAN BARU</h5>
+        <h5 class="title_1" >KOTA BEKASI</h5>
+        <div class="container">
+            <h2 class="title_2" style="margin-top:7px;">SELAMAT DATANG DI</h2>
+            <h2 class="title_3" style="margin-top:-10px;">SISTEM PELAYANAN SURAT PENGAJUAN</h2>
+        </div>
+        <div class="container mt-3" style="padding-left:175px;">
+            <div class="row">
+                <div class="col-md-12" class="gambar">
+                    <a href="../surat ket.usaha/ket_usaha.php" target="_blank" rel="noopener noreferrer"> 
+                        <img src="../image/1-2.jpg" alt="" id="logosurat">
+                    </a>
+                    <img src="../image/2-1.jpg" alt="" id="logosurat">
+                    <img src="../image/3-1.jpg" alt="" id="logosurat">
+                    <img src="../image/4-1.jpg" alt="" id="logosurat">
+                    <img src="../image/5-1.jpg" alt="" id="logosurat">
+                    <img src="../image/6-1.jpg" alt="" id="logosurat">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 mt-2">
+                    <img src="../image/7-1.jpg" alt="" id="logosurat">
+                    <img src="../image/8-1.jpg" alt="" id="logosurat">
+                    <img src="../image/9-1.jpg" alt="" id="logosurat">
+                    <img src="../image/10-1.jpg" alt="" id="logosurat">
+                    <img src="../image/11-1.jpg" alt="" id="logosurat">
+                    <img src="../image/12-1.jpg" alt=""id="logosurat">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="../bs/js/bootstrap.bundle.min.js"></script>
+    <script src="../jquery.min.js"></script>
+</body>
+</html>
---
+++ surat ket.usaha/ket_usaha.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shourcut icon" href="../image/logo pemkot bekasi-3.png">
+    <title>Kelurahan Harapan Baru</title>
+    <link rel="stylesheet" href="../bs/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="bg">
+        <img src="../image/logo pemkot bekasi-3.png" class="logo" alt="">
+        <img src="../image/lineup-3.png" class="lineup" alt="">         
+        <h5 class="title" >KELURAHAN HARAPAN BARU</h5>
+        <h5 class="title_1" >KOTA BEKASI</h5>
+        
+    </div>
+
+
+
+    <script src="../bs/js/bootstrap.bundle.min.js"></script>
+    <script src="../jquery.min.js"></script>
+</body>
+</html>
